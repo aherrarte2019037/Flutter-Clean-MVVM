0523145a1a4d728014e8f5dac3ad7c5fb38bb5c2
Angel <angelherrarte3@gmail.com>
2022-12-07 13:30:35 -0600

    Add linter rules and code metrics

---
+++ lib/application/application_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ApplicationApp extends StatefulWidget {
+  const ApplicationApp({super.key});
+  
+  @override
+  State<ApplicationApp> createState() => _ApplicationAppState();
+}
+
+class _ApplicationAppState extends State<ApplicationApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
