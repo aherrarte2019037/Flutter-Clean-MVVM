d4ac2575d4ea4e9ea60307a6ce016eeb1f0d91a0
Angel Herrarte <angelherrarte3@gmail.com>
2022-12-08 12:39:36 -0600

    Add font theme and assets gen

---
+++ lib/theme/font_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class FontTheme {
+  FontTheme._();
+  
+  static const TextStyle title10 = TextStyle(
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static const TextStyle body10 = TextStyle(
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+  static const TextStyle caption10 = TextStyle(
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+}
