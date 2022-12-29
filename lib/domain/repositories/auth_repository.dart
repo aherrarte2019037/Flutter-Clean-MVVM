8ca663cbae20246e7e75651e9ed40fde7b79e051
Angel <angelherrarte3@gmail.com>
2022-12-28 18:15:14 -0600

    Implement auth repository

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,4 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:tutapp/data/internet_status.dart';
+import 'package:tutapp/data/sources/auth_data_source.dart';
 import 'package:tutapp/domain/models/failure.dart';
 import 'package:tutapp/domain/models/login_credentials.dart';
 import 'package:tutapp/domain/models/login_result.dart';
@@ -7,4 +9,27 @@ abstract class AuthRepository {
   Future<Either<Failure, LoginResult>> login({
     required LoginCredentials credentials,
   });
+}
+
+class AuthRepositoryImpl implements AuthRepository {
+  const AuthRepositoryImpl(
+    this._authDataSource,
+    this._internetStatus,
+  );
+
+  final AuthDataSource _authDataSource;
+  final InternetStatus _internetStatus;
+
+  @override
+  Future<Either<Failure, LoginResult>> login({
+    required LoginCredentials credentials,
+  }) async {
+    final bool isConnected = await _internetStatus.isConnected;
+    if (!isConnected) return const Left(Failure(code: 409, message: 'Check your internet connection'));
+
+    final response = await _authDataSource.login(credentials);
+    if (response.status != 0) return const Left(Failure(code: 500, message: 'Server error'));
+
+    return Right(response.toDomain());
+  }
 }
